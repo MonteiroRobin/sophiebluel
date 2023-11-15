3fa519a93c7f7e3c3a1b8c3126d76f38c13da927
Robin Monteiro <robin@hesiode.io>
2023-11-15 15:15:17 +0100

    first push

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,38 +1,58 @@
-
+const categories = document.getElementById('categories');
 const works = document.getElementById('works');
 
-function buildWork(){
+
+
+function buildWork(work){
     
     const card = document.createElement('figure');
     const image = document.createElement('img');
     const caption = document.createElement('figcaption');
 
-    image.src = "aaa";
-image.setAttribute("alt", "de la photo");
-
-caption.innerHTML = "Titre de la photo";
+    image.src = work.imageUrl;
+    image.setAttribute("alt", work.title);
+    caption.innerHTML = work.title;
 
     card.appendChild(image);
     card.appendChild(caption);
     works.append(card);
 
+
 }
-
-buildWork()
-
-
-
-
 (async function(){
-     
-     
+
     const response = await fetch('http://localhost:5678/api/works')
-    const data = await response.json()
-    
-console.log(data)
+    const works = await response.json()
+    console.log(works);
+    for ( let work of works )
+    {
+        buildWork(work)
+    }
 })();
 
 
+function buildCategory(category){
+
+    const button = document.createElement('button');
+    button.textContent = category.name
+    button.classList.add('btn')
+    categories.appendChild(button)
+}
+
+(async function(){
+  
+    const response2 = await fetch('http://localhost:5678/api/categories')
+    const categories = await response2.json()
+
+  
+
+    console.log(categories);
+    for ( let category of categories )
+    {
+        buildCategory(category)
+    }
+})();
 
 
+//
 
